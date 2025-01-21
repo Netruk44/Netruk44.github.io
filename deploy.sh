11cfed107851e77c0e453870239b5c54036c04cc
Daniel Perry <dperry@danieltperry.me>
2025-01-21 18:34:35 -0500

    Script updates

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
-#! /bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
+PUBLIC_DIR="./public"
+
+# Remove public folder if it exists
+if [ -d "$PUBLIC_DIR" ]; then
+  rm -rf "$PUBLIC_DIR"
+fi
+
+# Build site with Hugo
 hugo --minify
-rclone sync ./public/ storage:/home/nginx/personal_site/ --progress
+
+# Sync generated site to remote server
+rclone sync "$PUBLIC_DIR" storage:/home/nginx/personal_site/ --progress
